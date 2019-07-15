b577790ed8167865b47fde176fa77ee12adbc65c
jashese <52932043+jashese@users.noreply.github.com>
2019-07-15 15:52:55 -0400

    Add files via upload

---
+++ python codes/7-15 python_coding.py
@@ -0,0 +1,42 @@
+print("Iam learning to code in python today")
+print(123)
+print("A B C, its easy as 123")
+print(07-15-2019)
+print("53")
+print(53)
+print(8/42)
+print(5*2)
+print(5/2)
+print(5^2)
+print(8/9)(3)
+print(5**2)
+print((8/9)*)3
+print(5**2)
+print((8/9)*3)
+print("the division of five times 2 = ") print(5/2)
+print(5/2)
+print("The division of 5/3 is" (5/3))
+'str'
+print("The division of 5/3, 5/3)
+print("the division of 5/3", 5/3)
+pwd
+#create function to sum any two numbers
+def add(a,b,c):
+    add = a + b + c
+    print(add)
+    
+add(2,3,5)
+def plus(a,b,c):
+add(3,2)
+add(1,2,3)
+def gamer(OWO):
+    gamer = This aint it cheif
+    print(gamer)
+def multiply(a,b,c,d,e):
+    multiply = a * b * c * d * e
+    print(multiply)
+        
+multiply(1,1,1,1,2)        
+
+
+tri(1/2,2,4)
