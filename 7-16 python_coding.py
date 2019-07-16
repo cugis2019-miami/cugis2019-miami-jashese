9c7546e1ac147f7cef2dfa6c2bbe15f1428201e5
jashese <52932043+jashese@users.noreply.github.com>
2019-07-16 16:03:24 -0400

    Add files via upload

---
+++ 7-16 python_coding.py
@@ -0,0 +1,152 @@
+print("box",=5)print("there are", box,"box in the box")
+box=5
+p
+box=5
+box=box+5
+print("there are",box,"bars in the box")
+
+#CHOCLATE!!!
+  D = "Dark choclate"
+  M = "Milk choclate"
+  W = "White choclate"
+
+
+print(D,M,W)
+print(D,,M,,W)
+"D" = dark
+
+#function that greets people
+def hello_siri(a):
+    a = (name)
+    print("hello",a)
+
+
+
+
+
+hello_siri(bobert)
+
+
+hello_siri()
+hello_siri(a)
+hello_siri(2)
+
+hello_siri("bob")
+hello_siri bob
+hello_siri(bob)
+hello_siri("bobert")
+
+
+
+
+import math
+22/7
+dir(math)
+
+math.pi
+math.sqrt(10)
+math.factorial(0)
+
+math.pow
+math.pow(4**7)
+
+math.pow(4)(7)
+dir(math)
+import math
+dir(math)
+math.pow(4)(5)
+math.remainder(12,7)
+12/7
+math.pow(1,0)
+math.remainder(5,3)
+5/3
+
+def cube(a):
+    cube = a / a / a
+    print(cube)
+
+cube(15,3)
+
+
+
+math.
+math.factorial(125)
+
+
+
+
+
+cube(125)
+
+
+math.pow(5,3)
+math.pow(125)
+def cube(y):
+    cube = math.pow(y,1/3)
+    print("the cube of", y,"is ",cube)
+    cube = a * 1/3
+    print(cube)
+
+
+cube(125)
+
+5*5*5
+
+
+print("Please enter a value to the variable j .")
+
+
+
+
+
+def 3root(j):
+    j = 8
+    
+
+
+
+
+
+
+
+
+
+
+def cube(x):
+    cube = math.pow(x,1/3)
+    print("the cube root of",x,"is",cube)
+
+cube(27)
+
+
+
+import math
+print("please enter a value")
+y = 20
+
+cube = math.pow(y,(1/3))
+print("the cube root of",y,"is",cube)
+
+q = "please enter a value to the variable t"
+print(q,)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
