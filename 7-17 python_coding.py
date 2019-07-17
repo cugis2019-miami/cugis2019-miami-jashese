e4225eb870e6dc326f399c21ad423bdb851b3c7d
jashese <52932043+jashese@users.noreply.github.com>
2019-07-17 16:00:41 -0400

    Add files via upload

---
+++ 7-17 python_coding.py
@@ -0,0 +1,77 @@
+#7-17 spyder python ide    (cugis miami)
+
+d = "dark"
+m = "milk" 
+w = "white"
+
+
+
+print("There are",3,d,5,m,"and",8,w,"choclate bars in the box")
+
+#dict data structure
+chocolate1 = {"milk":5}
+chocolate2 = {"dark":3}
+chocolate3 = {"white":8}
+
+chocolatebox = {"milk":5,"dark":3,"white":8}
+print(chocolatebox)"M","Lia":F,"Vin":M,"Katie":F}
+print("the types and number of choclate in the boxis",chocolatebox)
+
+
+
+steve = {}
+
+gender = {"Steve":"Male","Lia":"Female","Vin":"Male","Katie":"Female"}
+
+
+
+age 
+
+age
+age gender
+age
+gender
+chocolatebox = {"milk":5,"dark":3,"white":8}
+print("the number of dark chocolate in chocolate box is",
+      chocolatebox["milk"])
+
+print("Lia's age is",
+      age["Lia"],
+      "and gender is",
+      gender["Lia"])
+      
+     
+import pandas
+dir(pandas)
+pandas.Series()
+
+
+
+
+chocodata = [choco]
+print(chocodata)
+
+
+chocodf = pandas.DataFrame(chocodata
+print(chocodf)
+
+
+agedata = [age]
+print(agedata)
+
+aged
+agedf = pandas.DataFrame("agedate")
+print(agedf)
+
+studentallinfo = [["steve",32,"male"],["lia",28,"female"],["vin",45,"male"],["katie",38,"female"]]
+print(studentallinfo)
+
+df = pandas.DataFrame(studentallinfo)
+print(df)
+\
+
+df2= pandas.DataFrame(studentallinfo, columns=["Name","Age", "Gender"])
+print(df2)
+
+
+
